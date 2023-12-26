66a656158fdeb47bf3e7d1b95a91e4aef716a650
Madhavkalia076 <madhavkalia054@gmail.com>
2023-12-26 17:28:25 +0530

    first

---
+++ LEETCODE/majority_element_II.cpp
@@ -0,0 +1,38 @@
+// Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+
+vector<int> majorityElement(vector<int>& nums) {
+        map<int,int>frequency;
+        for(int i=0;i<nums.size();i++){
+            if(frequency.find(nums[i])==frequency.end()){
+                   frequency[nums[i]]=1;
+            }
+            else{
+                frequency[nums[i]]++;
+            }
+        }
+       
+       
+        vector<int> result;
+        for(auto&pair:frequency){
+            if( pair.second>(nums.size()/3)){
+                
+                result.push_back(pair.first);
+            }
+        }
+        return result;
+    }
+int main(){
+  vector<int>nums={1,2,2,1,1,3};
+  vector<int>ans;
+  ans=majorityElement(nums);
+  for(int i=0;i<ans.size();i++){
+    cout<<ans[i]<<" ";
+  }
+  return 0;
+
+}
