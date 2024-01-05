7f2f93b695fb04042ff3a7b5c20469783b4177f5
Madhavkalia076 <madhavkalia054@gmail.com>
2024-01-05 21:16:01 +0530

    modified

--- LEETCODE/single_element.cpp
+++ LEETCODE/single_element.cpp
@@ -34,7 +34,7 @@ using namespace std;
         return -1;
     }
     int main(){
-        vector<int>nums={1,1,2,3,3,4,4,8,8};
+        vector<int>nums={3,3,7,7,10,11,11};
         cout<<"single element in the given array: "<<singleNonDuplicate(nums);
         return 0;
     }
