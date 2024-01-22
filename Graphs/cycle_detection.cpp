abee152b991719dcc0b773258b0f17f997b7a867
Madhavkalia076 <madhavkalia054@gmail.com>
2024-01-22 15:54:24 +0530

    modified

--- Graphs/cycle_detection.cpp
+++ Graphs/cycle_detection.cpp
@@ -1,70 +1,73 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <queue>
-#include <list>
+// Given an undirected graph with V vertices labelled from 0 to V-1 and E edges, check whether it contains any cycle or not. Graph is in the form of adjacency list where adj[i] contains all the nodes ith node is having edge with.
+
+#include <bits/stdc++.h>
 using namespace std;
 
-bool iscyclic(int src, map<int, bool> &visited, map<int, list<int>> &adj)
+class Solution
 {
-  map<int, int> parent;
-  parent[src] = -1;
-  visited[src] = 1;
-  queue<int> q;
-  q.push(src);
-
-  while (!q.empty())
+private:
+  bool detect(int src, vector<int> adj[], int vis[])
   {
-    int front = q.front();
-    q.pop();
-
-    for (auto i : adj[front])
+    vis[src] = 1;
+    // store <source node, parent node>
+    queue<pair<int, int>> q;
+    q.push({src, -1});
+    // traverse until queue is not empty
+    while (!q.empty())
     {
-      if (visited[i] == true && i != parent[front])
+      int node = q.front().first;
+      int parent = q.front().second;
+      q.pop();
+
+      // go to all adjacent nodes
+      for (auto adjacentNode : adj[node])
       {
-        return true;
-      }
-      else if (!visited[i])
-      {
-        q.push(i);
-        visited[i] = 1;
-        parent[i] = front;
+        // if adjacent node is unvisited
+        if (!vis[adjacentNode])
+        {
+          vis[adjacentNode] = 1;
+          q.push({adjacentNode, node});
+        }
+        // if adjacent node is visited and is not it's own parent node
+        else if (parent != adjacentNode)
+        {
+          // yes it is a cycle
+          return true;
+        }
       }
     }
-  }
-  return false;
-}
-
-string cycleDetection(vector<vector<int>> &edges, int n, int m)
-{
-
-  map<int, list<int>> adj;
-  for (int i = 0; i < edges.size(); i++)
-  {
-    int u = edges[i][0];
-    int v = edges[i][1];
-    adj[u].push_back(v);
-    adj[v].push_back(u);
+    // there's no cycle
+    return false;
   }
 
-  map<int, bool> visited;
-  for (int i = 0; i < n; i++)
+public:
+  // Function to detect cycle in an undirected graph.
+  bool isCycle(int V, vector<int> adj[])
   {
-    if (!visited[i])
+    // initialise them as unvisited
+    int vis[V] = {0};
+    for (int i = 0; i < V; i++)
     {
-      bool ans = iscyclic(i, visited, adj);
-      if (ans == 1)
+      if (!vis[i])
       {
-        return "Yes";
+        if (detect(i, adj, vis))
+          return true;
       }
     }
+    return false;
   }
-  return "No";
-}
+};
+
 int main()
 {
-  vector<vector<int>> edges = {{1, 2}, {2, 3}};
-  string ans = cycleDetection(edges, 3, 2);
-  cout << "Is graph cyclic: " << ans;
+
+  // V = 4, E = 2
+  vector<int> adj[4] = {{}, {2}, {1, 3}, {2}};
+  Solution obj;
+  bool ans = obj.isCycle(4, adj);
+  if (ans)
+    cout << "1\n";
+  else
+    cout << "0\n";
   return 0;
 }
