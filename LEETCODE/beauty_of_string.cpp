b943dccedcdcbb8c6a722a22b697019af180641c
Madhavkalia076 <madhavkalia054@gmail.com>
2024-01-26 20:37:40 +0530

    modified

--- LEETCODE/beauty_of_string.cpp
+++ LEETCODE/beauty_of_string.cpp
@@ -1,3 +1,8 @@
+// The beauty of a string is the difference in frequencies between the most frequent and least frequent characters.
+
+// For example, the beauty of "abaacc" is 3 - 1 = 2.
+// Given a string s, return the sum of beauty of all of its substrings.
+
 #include <iostream>
 #include <string>
 #include <unordered_map>
