67fd6e884c37ac03c92fa04fa535867b03b6eddb
Madhavkalia076 <madhavkalia054@gmail.com>
2024-02-08 22:51:47 +0530

    new

---
+++ LEETCODE/power_of_two.cpp
@@ -0,0 +1,25 @@
+// Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+// An integer n is a power of two, if there exists an integer x such that n == 2x
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+bool isPowerOfTwo(int n)
+{
+  for (int i = 0; i <= 30; i++)
+  {
+    int ans = pow(2, i);
+    if (ans == n)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+int main()
+{
+  int n = 27;
+  cout << isPowerOfTwo(n);
+  return 0;
+}
